570d3576b91f8353cbe6649faefe7f7e2aaa4207
ilan grodinsky <ilan7gr@gamil.com>
2024-03-19 16:53:55 +0200

    created a window

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log('hello world!');
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const isDev = process.env.NODE_ENV !== 'production';
+const isMac = process.platform === 'darwin';
+
+// Create the main application window
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        },
+    });
+  
+    win.loadFile(path.join(__dirname, 'index.html'));
+}
+
+app.whenReady().then(() => {
+    createWindow()
+    
+    // Open a window if none are open (macOS)
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+})
+
+// Close the app when all windows are closed (not macOs)
+app.on('window-all-closed', () => {
+    if (!isMac) app.quit();
+});
