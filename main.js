09b35d6ec12c64f73738e40703e1e5268a5d026f
kneecapFolder <ilan7gr@gmail.com>
2024-03-31 23:59:55 +0300

    finished the game loop

--- main.js
+++ main.js
@@ -1,3 +1,43 @@
+class Option {
+    constructor(value, id) {
+        this.value = value;
+        this.id = id;
+        this.optElm;
+
+        this.add();
+    }
+
+    add() {
+        this.optElm = document.createElement('div');
+        this.optElm.className = 'option';
+        this.optElm.innerText = this.value;
+        this.optElm.onclick = () => this.click();
+
+        DOM.get('quiz').children[1].appendChild(this.optElm);
+    }
+
+    async click() {
+        if (!interactable) return;
+        interactable = false;
+
+        // Reveal the correct answer
+        this.optElm.classList.add('revealed');
+        let correct = DOM.get('quiz').children[1].children[answers[qIndex]-1];
+        correct.classList.add('revealed');
+        correct.style.backgroundColor = 'lightgreen';
+
+        // Wait for a bit before load the next question
+        await new Promise(res => setTimeout(res, 1000));
+        if (this.id !== answers[qIndex]-1) {
+            display('start');
+            return;
+        }
+        qIndex++;
+        if (qIndex >= questions.length) display('end');
+        else display('pre');
+    }
+}
+
 // DOM
 const DOM = new Map()
     .set('start', document.getElementById('start'))
@@ -13,7 +53,6 @@ fileInput.onchange = async () => {
     reader.readAsText(fileInput.files[0]);
     reader.onload = async (e) => {
         quiz = await JSON.parse(e.target.result);
-
         load();
     };
 }
@@ -24,9 +63,11 @@ var preText;
 var questions;
 var options;
 var answers;
+var qIndex = 0;
+var interactable = true;
 
 async function start() {
-    quiz = await (await fetch('./quiz.json')).json();
+    quiz = await (await fetch('./quiz-template.json')).json();
     
     load();
 }
@@ -55,6 +96,19 @@ function display(state) {
             elm.children[1].innerText = quiz.startPre;
             elm.children[4].innerText = fileInput.files.length ? fileInput.files[0].name : 'no file selected';
             elm.children[8].innerText = quiz.credit;
+
+            qIndex = 0;
+            interactable = true;
+        break;
+        case 'pre':
+            elm.children[0].innerText = preText[qIndex];
+
+            interactable = true;
+        break;
+        case 'quiz':
+            elm.children[0].innerText = questions[qIndex];
+            elm.children[1].innerHTML = '';
+            options[qIndex].forEach((value, id) => new Option(value, id));
         break;
     }
 }
